c6fb8fa939d79eb531a3c5ee9642bc000c81f191
James McDuffie <James.McDuffie@jpl.nasa.gov>
2022-07-12 11:07:06 -0700

    Remove extra space

--- dev_env/support_instance/key_pair.tf
+++ dev_env/support_instance/key_pair.tf
@@ -8,7 +8,6 @@ resource "aws_key_pair" "app_dev_support_key_pair" {
   public_key = tls_private_key.app_dev_support_priv_key.public_key_openssh
 }
 
-
 output "private_key" {
   value     = tls_private_key.app_dev_support_priv_key.private_key_pem
   sensitive = true
