c2d93b0fbe973662c78abbbb9112f36b9d55077b
Lahaye <Nicholas.J.LaHaye@jpl.nasa.gov>
2023-01-27 19:54:54 -0800

    Updating DB config/scripts

--- application_package/dockstore/database/database-dev.tf
+++ application_package/dockstore/database/database-dev.tf
@@ -1,6 +1,14 @@
 resource "aws_cloudformation_stack" "db" {
   name = "awsDbDockstoreStack"
 
+   parameters = {
+    ResourcePrefix = "${var.resource_prefix}"
+    DBName = "${var.resource_prefix}"
+    DBMasterUserPassword  = "/DeploymentConfig/${var.resource_prefix}/DBPostgresPassword"
+    VpcId = data.aws_vpc.unity_vpc.id
+    SubnetId1 = tolist(data.aws_subnets.unity_public_subnets.ids)[0]
+    SubnetId2 = tolist(data.aws_subnets.unity_public_subnets.ids)[1]
+  }
 
   template_body = file("./database-dev.yml")
 
