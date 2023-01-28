ced17ff664a5249f699bc3e06ac54ba0c931dbe2
Lahaye <Nicholas.J.LaHaye@jpl.nasa.gov>
2023-01-27 20:07:59 -0800

    Updating ES Log Group config/scripts

--- application_package/dockstore/es-log-groups/es-log-groups-dev.tf
+++ application_package/dockstore/es-log-groups/es-log-groups-dev.tf
@@ -10,3 +10,26 @@ resource "aws_cloudformation_stack" "es_log_groups" {
 
 }
 
+data "aws_iam_policy_document" "dockstore-elasticsearch-application-logs" {
+  statement {
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:PutLogEventsBatch",
+    ]
+
+    resources = ["arn:aws:logs:*"]
+
+    principals {
+      identifiers = ["es.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
+
+resource "aws_cloudwatch_log_resource_policy" "elasticsearch-log-publishing-policy" {
+  policy_document = data.aws_iam_policy_document.dockstore-elasticsearch-application-logs.json
+  policy_name     = "dockstore-elasticsearch-application-logs"
+}
+
+
