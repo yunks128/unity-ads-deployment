21d4477f267822bad71d3928487717b297841f35
Lahaye <Nicholas.J.LaHaye@jpl.nasa.gov>
2023-02-03 01:36:20 -0800

    Fixing issues with elasticsearch and lambda

--- application_package/dockstore/initial_deploy/s3/sed_command.sh
+++ application_package/dockstore/initial_deploy/s3/sed_command.sh
@@ -2,8 +2,8 @@
 
 newip=`ifconfig  | grep -Eo '([0-9]*\.){3}[0-9]*' |  grep -v ^172 | grep -v 255 | grep -v ^127 | xargs echo` 
 sed -i "s/10.52.14.72/${newip}/" config/default.nginx_http.*
-sed -i "s/  hostname:.*/  hostname: uads-test-dockstore/" config/web.yml
-
+sed -i "s/  hostname:.*http.*/  hostname: uads-test-dockstore/" config/web.yml
+sed -i "s/.*ES_DELETE.*//g" config/web.yml
 
 
 
