a067c095ecba15bdda19adf3fe9c1b1c4c9b0e55
Lahaye <Nicholas.J.LaHaye@jpl.nasa.gov>
2023-02-02 22:36:55 -0800

    Fixing bugs

--- application_package/dockstore/initial_deploy/s3/sed_command.sh
+++ application_package/dockstore/initial_deploy/s3/sed_command.sh
@@ -2,5 +2,8 @@
 
 newip=`ifconfig  | grep -Eo '([0-9]*\.){3}[0-9]*' |  grep -v ^172 | grep -v 255 | grep -v ^127 | xargs echo` 
 sed -i "s/10.52.14.72/${newip}/" config/default.nginx_http.*
-sed -i "s/  hostname:.*http/uads-test-dockstore/" config/web.yml
+sed -i "s/  hostname:.*/  hostname: uads-test-dockstore/" config/web.yml
+
+
+
 
