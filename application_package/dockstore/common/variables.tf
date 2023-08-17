841c993a933becfd6d715bde74c969a992aded0e
Masha Liukis <mliukis@jpl.nasa.gov>
2023-08-17 14:47:57 -0700

    Remove default values where environment specific values are expected

--- application_package/dockstore/common/variables.tf
+++ application_package/dockstore/common/variables.tf
@@ -1,13 +1,11 @@
 variable "unity_instance" {
   description = "Name of the Unity instance where deploying"
   type        = string
-  default     = "Unity-Dev"
 }
 
 variable "resource_prefix" {
   description = "String used at the beginning of the names for all resources to identify them according to the UADS subsystem"
   type        = string
-  default     = "dev"
 }
 
 variable "api_id" {
@@ -22,24 +20,23 @@ variable "api_parent_id" {
 
 variable "availability_zone" {
   description = "The availability zone for the RDS DB"
-  type = string
-  default = "us-west-2b"
+  type        = string
 }
 
 variable "lb_logs_bucket_name" {
-  description = "The name of manually created S3 bucket to store Load Balancer logs"
-  type = string
-  default = "uads-dev-dockstore-elb-logs"
+  description = "The name of manually created (by MCP Help) S3 bucket to store Load Balancer logs"
+  type        = string
+  default     = "uads-dev-dockstore-elb-logs"
 }
 
 variable "lb_logs_bucket_prefix" {
-  description = "The prefix for the location in the S3 bucket for the Load Balancer access logs"
-  type = string
-  default = "AccessLogs"
+  description = "The prefix for the location (as specified by the S3 bucket policy when creating the bucket by MCP Help) in the S3 bucket for the Load Balancer access logs"
+  type        = string
+  default     = "AccessLogs"
 }
 
 variable "db_snapshot" {
   description = "AWS ARN of the RDB snapshot to restore new deployment of the database from"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
