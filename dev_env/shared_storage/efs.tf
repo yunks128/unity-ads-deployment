a068c6c43e6ea0caddf292da6b20b3fd5e9d224d
James McDuffie <James.McDuffie@jpl.nasa.gov>
2023-11-10 11:20:59 -0800

    Add creation_info to EFS access point to ensure it gets created when not already existing.

--- dev_env/shared_storage/efs.tf
+++ dev_env/shared_storage/efs.tf
@@ -11,5 +11,10 @@ resource "aws_efs_access_point" "jupyter_shared" {
   file_system_id = aws_efs_file_system.dev_support_efs.id
   root_directory {
     path = "/shared"
+    creation_info {
+       owner_uid = 0
+       owner_gid = 0
+       permissions = 755
+    }
   }
 }
