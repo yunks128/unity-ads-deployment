b8e3bac05c2d2a44e09f61954cef2019d0d2fa3b
James McDuffie <James.McDuffie@jpl.nasa.gov>
2024-01-18 11:34:22 -0800

    Add example environment variable definition for Jupyterhub

---
+++ dev_env/vars_jupyter.example.sh
@@ -0,0 +1,12 @@
+export AWS_PROFILE=mcp-venue-dev
+
+export KUBE_CONFIG_PATH=$HOME/.kube/config
+export TF_VAR_unity_instance="Unity-Venue-Dev"
+export TF_VAR_tenant_identifier="example-dev"
+export TF_VAR_s3_identifier="dev"
+export TF_VAR_efs_identifier="uads-venue-dev-efs-fs"
+export TF_VAR_cognito_oauth_base_url="https://unitysds.auth.us-west-2.amazoncognito.com"
+export TF_VAR_cognito_oauth_client_id=""
+export TF_VAR_cognito_oauth_client_secret=""
+#export TF_VAR_jupyter_s3_buckets='["bucket-name-1", "bucket-name-2"]'
+#export TF_VAR_jupyter_admin_users='[]'
