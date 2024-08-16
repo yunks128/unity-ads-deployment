c31eca80fa456d566192c55d12aa4b3985c2bfc3
James McDuffie <James.McDuffie@jpl.nasa.gov>
2024-08-16 15:14:32 -0700

    Enable encryption on Jupyter EFS

--- dev_env/shared_storage/efs.tf
+++ dev_env/shared_storage/efs.tf
@@ -1,6 +1,23 @@
+resource "aws_kms_key" "efs_key" {
+  description             = "KMS key for Jupyter ${var.venue_prefix}${var.venue} EFS encryption"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+
+  tags = {
+    Name = "${var.resource_prefix}-EfsKmsKey-${var.venue_prefix}${var.venue}"
+  }
+}
+
+resource "aws_kms_alias" "efs_key_alias" {
+  name          = "alias/${var.resource_prefix}-${var.venue_prefix}${var.venue}-efs-key"
+  target_key_id = aws_kms_key.efs_key.key_id
+}
+
 resource "aws_efs_file_system" "dev_support_efs" {
-   creation_token = "efs"
+   creation_token   = "efs"
    performance_mode = "generalPurpose"
+   encrypted        = true
+   kms_key_id       = aws_kms_key.efs_key.arn
 
    tags = {
      Name = "${var.efs_identifier}"
